ff2afa86e0fad65a66ff6b184004a8dd42534eb4
Student <example@example.com>
2019-10-30 11:11:41 -0700

    add more js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
+
+    var word = $("#word").val()
     event.preventDefault();
-    
+
+    var a = word.split('')
+
+    for (var i = 0; i < word.length; i++) {
+      // if (a[i] == "a" || a[i] == "e" || a[i] == "i" ||a[i] == "o" ||a[i] == "u")
+      // a[i] = "-"
+
+
+     if (a[i] == "!" || a[i] == "?" ) {
+       alert("please use letters only");
+     } else if () {
+
+     }
+
+
+
+
+    }
+
+  console.log(a)
+   var newWord = a.join('')
+
+   console.log(newWord);
+
   })
-
-
-
-
-
 
 
 })
